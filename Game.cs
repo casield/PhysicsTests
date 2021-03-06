e26cd05e02c575b28da1af6fb6c21c605afd3702
Casiel <casield@gmail.com>
2021-03-05 18:40:08 -0600

    first commit

---
+++ Game.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuPhysics.CollisionDetection;
+using BepuPhysics.Constraints;
+using BepuUtilities;
+using BepuUtilities.Memory;
+
+
+namespace TEs_Physics
+{
+    class Game
+    {
+        private BodyDescription boxDescription;
+        private BodyHandle bodyHandle;
+
+        public Simulation Simulation { get; }
+        public IThreadDispatcher ThreadDispatcher { get; private set; }
+
+        public Game()
+        {
+            Console.WriteLine("Inside Game");
+            BufferPool bufferPool = new BufferPool();
+
+            var targetThreadCount = Math.Max(1, Environment.ProcessorCount > 4 ? Environment.ProcessorCount - 2 : Environment.ProcessorCount - 1);
+            ThreadDispatcher = new SimpleThreadDispatcher(targetThreadCount);
+
+            Simulation = Simulation.Create(bufferPool, new DemoNarrowPhaseCallbacks(), new DemoPoseIntegratorCallbacks(new Vector3(0, -10, 0)), new PositionFirstTimestepper());
+            createFloor();
+            createObjects();
+
+            GameLoop gameloop = new GameLoop();
+            gameloop.Load(this);
+            gameloop.Start().Wait();
+        }
+
+        private void createObjects()
+        {
+            var ringBoxShape = new Box(0.5f, 1, 3);
+            ringBoxShape.ComputeInertia(1, out var ringBoxInertia);
+            boxDescription = BodyDescription.CreateDynamic(new Vector3(), ringBoxInertia,
+                new CollidableDescription(Simulation.Shapes.Add(ringBoxShape), 0.1f),
+                new BodyActivityDescription(0.01f));
+
+            boxDescription.Pose = new RigidPose(new Vector3(1, 9, 1), new Quaternion(0, 0, 0, 1));
+           bodyHandle = Simulation.Bodies.Add(boxDescription);
+        }
+
+        private void createFloor()
+        {
+            Simulation.Statics.Add(new StaticDescription(new Vector3(0, -0.5f, 0), new CollidableDescription(Simulation.Shapes.Add(new Box(500, 1, 500)), 0.1f)));
+        }
+
+        internal void Load()
+        {
+            Console.WriteLine("Load content");
+        }
+
+        internal void Update(TimeSpan time)
+        {
+            Simulation.Timestep(1 / 60f, ThreadDispatcher);
+            Simulation.Bodies.GetDescription(bodyHandle, out boxDescription);
+            Console.WriteLine(boxDescription.Pose.Position);
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuPhysics.CollisionDetection;
+using BepuPhysics.Constraints;
+using BepuUtilities.Memory;
+
+namespace TEs_Physics
+{
+    class Program
+    {
+        public BufferPool BufferPool { get; private set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            new Game();
+        }
+    }
+}
